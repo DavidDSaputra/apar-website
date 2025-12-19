5d8bd08e422df25190e40a46dffd2d49822b1bce
NamaAnda <julianarwansahh@gmail.com>
2025-12-19 09:48:58 +0700

    feat: Add initial web routes for home and apar pages.feat: Add initial web routes for home and apar pages.

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,6 @@
 use App\Http\Controllers\AparController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [AparController::class, 'index'])->name('home');
 
 Route::get('/apar', [AparController::class, 'index'])->name('apar.index');
